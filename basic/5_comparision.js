270832f9cf98b8add1d8144404c0a2d4679f760e
Manoj <119852764+mjmohanty0704@users.noreply.github.com>
2024-03-11 06:55:25 +0000

    rename

---
+++ basic/5_comparision.js
@@ -0,0 +1,15 @@
+let num = "A";
+// console.log(num.charCodeAt()); //to retrieve the ASCII
+
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined == 0);
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+// === strict comprision; both type and value
+
+console.log(typeof (Number(null) === 0));
+console.log(typeof null);
