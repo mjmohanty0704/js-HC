18e6de34ee5d537064471edb2b2c15b2faaac573
Manoj <119852764+mjmohanty0704@users.noreply.github.com>
2024-03-11 10:16:22 +0000

    date and time

---
+++ basic/10_date_and_time.js
@@ -0,0 +1,25 @@
+let date = new Date();
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+
+// let myDate = new Date(1999,3,7);
+// console.log(myDate.toDateString());
+
+// myDate = new Date(1999,3,7,16,20);
+// console.log(myDate.toLocaleString());
+
+// let createDate = new Date("2013-01-14");
+// console.log(createDate.toDateString());
+
+// let createDate = new Date("07-04-1999");
+// console.log(createDate.toLocaleString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(createDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate.getDate(),'/',newDate.getMonth()+1,'/',newDate.getFullYear());
