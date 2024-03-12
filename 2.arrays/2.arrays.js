8d98c74fdaca9408ea029873bcc777ea5226c0ef
Manoj <119852764+mjmohanty0704@users.noreply.github.com>
2024-03-12 06:23:00 +0000

    methods of arrays

---
+++ 2.arrays/2.arrays.js
@@ -0,0 +1,23 @@
+const marvelHeros = ["Iron-Man","Thor","Spider-Man"];
+// const DCHeros = ["Superman","Flash","Batman"];
+// console.log(Array.of(...marvelHeros,...DCHeros));
+
+// marvelHeros.push(DCHeros);
+
+// console.log(marvelHeros[3][2]);
+// console.log(marvelHeros.concat(DCHeros));
+
+// const allNewHeros = [...marvelHeros,...DCHeros];
+// console.log(allNewHeros);
+
+// const anotherArr = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+// const realAnotherArr = anotherArr.flat(Infinity);
+// console.log(realAnotherArr);
+
+// console.log(Array.isArray("Manoj"));
+// console.log(Array.from(marvelHeros[2]));
+
+let a = 20;
+let b = 30;
+let c = 40;
+console.log(Array.of(a,b,c));
